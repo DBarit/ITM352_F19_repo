778206f7583b3a29cf9449137cd8d7946cff079a
DBarit <danielbarit@gmail.com>
2019-10-10 00:34:01 -1000

    Add files via upload

---
+++ Lab9/Ex1.js
@@ -0,0 +1,21 @@
+//Exercise 1: Algorithms
+
+date = "6/7/1999"; 
+
+step1 = 99; //result = 99
+
+step2 = parseInt(step1/4); //result = 24
+
+step3 = step2 + step1; //result = 123
+
+step4 = 4; //result = 4
+
+step6 = step4 + step3; //result = 127
+
+step7 = step6 + 7; //result = 134
+
+step8 = 134;
+
+dayBorn = step8%7; //result = 1
+
+console.log(dayBorn);
